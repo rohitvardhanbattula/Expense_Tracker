a83c14e64397fe7fdd6312dd16f9c4d1e5afe784
rohitvardhanbattula <vardhanbattula03@gmail.com>
2026-04-21 11:10:00 +0530

    fix(backend): production deployment

--- backend/server.js
+++ backend/server.js
@@ -69,4 +69,5 @@ app.get('/expenses', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Backend running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
